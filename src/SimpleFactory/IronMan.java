57d04bc677a46de3c4f807234e9e9f0e07b5d5fd
5Embp <iting0317@gmail.com>
2017-12-14 15:44:38 +0800

    補回@Override的注釋
    發現@Override的功能，是讓編譯器幫忙檢查是否正確override了父類別的方法，還是有打錯字。

--- src/SimpleFactory/IronMan.java
+++ src/SimpleFactory/IronMan.java
@@ -2,14 +2,17 @@ package SimpleFactory;
 
 public class IronMan implements IHeroToys{
 
+	@Override
 	public void showSpecification() {
 		System.out.println("This is a IronMan.");
 	}
 
+	@Override
 	public void run() {
 		System.out.println("IronMan can fly.");
 	}
-
+	
+	@Override
 	public void showPublisher() {
 		System.out.println("IronMan belongs to Marvel.");
 	}
--- src/SimpleFactory/SpiderMan.java
+++ src/SimpleFactory/SpiderMan.java
@@ -2,14 +2,17 @@ package SimpleFactory;
 
 public class SpiderMan implements IHeroToys{
 
+	@Override
 	public void showSpecification() {
 		System.out.println("This is a SpiderMan.");
 	}
 
+	@Override
 	public void run() {
 		System.out.println("SpiderMan can't fiy.");
 	}
 
+	@Override
 	public void showPublisher() {
 		System.out.println("SpiderMan belongs to Sony by 2015.");
 	}
