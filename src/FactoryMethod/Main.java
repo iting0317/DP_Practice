2eaadd1ba64d5571a84ef5cb9de21f9262012a91
5Embp <iting0317@gmail.com>
2017-12-16 18:18:17 +0800

    主程式新增一個SpiderManDeluxeEdition訂單

--- src/FactoryMethod/Main.java
+++ src/FactoryMethod/Main.java
@@ -5,15 +5,14 @@ import FactoryMethod.ToysFactory.ToysType;
 public class Main {
 
 	public static void main(String[] args) {
-		// 宣告一個玩具工廠實例
+		//鋼鐵人玩具工廠
 		ToysFactory ironManToyFacory = new IronManFactory();
-
-		// 由工廠生產玩具的實例
 		Toys ironManToy = ironManToyFacory.orderToys(ToysType.IronMan);
 		
+		//蜘蛛人玩具工廠
 		ToysFactory spiderManToyFacory = new SpiderManFactory();
 		Toys spiderManToy = spiderManToyFacory.orderToys(ToysType.SpiderMan);
-
+		Toys spiderManDeluxeEdition = spiderManToyFacory.orderToys(ToysType.SpiderManDeluxeEdition);
 
 	}
 
